757f8d42d82b6e38e796f697321f74a1edfe1e63
tahatech <thhk2831@gmail.com>
2024-07-05 23:34:28 +0330

    publish

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advanced PHP MVC Blog
+
+A simple blog written in php with advanced use of oop with different design patterns
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'server'    => 'k2.liara.cloud',
+    'port'      => '34265',
+    'dbname'    => 'blog',
+    'dbuser'    => 'root',
+    'dbpass'    => 'nHw7xpIbxA5PPJLZkZmjU7Bq',
+];
